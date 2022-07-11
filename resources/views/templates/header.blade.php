48707e8da1c2c1159a70cd8f7a3704f6d6a9d210
Felix Kiptoo <felixkpt@gmail.com>
2022-07-11 16:06:15 +0300

    edited favicon link

--- resources/views/templates/header.blade.php
+++ resources/views/templates/header.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="{{ $description ?? \SiteInfo::description() }}">
     <title>{{ $title ?? \SiteInfo::title() }}</title>
-    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ url('favicon.ico') }}" type="image/x-icon">
     <link rel="stylesheet" href="{{ asset('css/style.css?v=').date('Y.m.d-H:i:s') }}">
     <script src="{{ asset('js/script.js?v=').Str::random(10) }}" async></script>
     <script src="{{ asset('js/flowbite.js') }}"></script>
